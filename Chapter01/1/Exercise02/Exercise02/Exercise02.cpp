071554cd16583169f5e3725aacaa29fe6b925688
BOOK-36O9DT5K00\wonyong <r00tdr4g0n@gmail.com>
2024-05-12 23:59:21 +0900

    [add] Chapter01 - 1 - Exercise02 추가

---
+++ Chapter01/1/Exercise02/Exercise02/Exercise02.cpp
@@ -0,0 +1,62 @@
+﻿#include "framework.h"
+#include "Exercise02.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+WCHAR szWindowClass[] = L"WindowClass Name";
+WCHAR szTitle[] = L"r00tdr4g0n의 첫 번째 윈도우";
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    HWND hWnd;
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_QUESTION);
+    wcex.hCursor = LoadCursor(NULL, IDC_IBEAM);
+    wcex.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(NULL, IDI_QUESTION);
+    RegisterClassExW(&wcex);
+
+    hWnd = CreateWindow(szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        200, 300, 600, 400,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message) {
+    case WM_CREATE:
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
