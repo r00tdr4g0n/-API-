76079ad3e806a5b826a5930ae5dbfa02a0b75326
BOOK-36O9DT5K00\wonyong <r00tdr4g0n@gmail.com>
2024-05-15 19:12:28 +0900

    [add] Chapter02 Exercise02 추가

---
+++ Chapter02/Exercise02/Exercise02/Exercise02.cpp
@@ -0,0 +1,81 @@
+﻿#include "framework.h"
+#include "Exercise02.h"
+#include "ctime"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+WCHAR szWindowClass[] = L"WindowClass Name";
+WCHAR szTitle[] = L"Chapter02 Exercise02";
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    HWND hWnd;
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    RegisterClassExW(&wcex);
+
+    hWnd = CreateWindow(szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 600, 400,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    PAINTSTRUCT ps = { 0, };
+    HDC hdc = NULL;
+    static TCHAR str[100] = { 0, };
+
+    srand((unsigned int)time(NULL));
+
+    switch (message) {
+    case WM_CREATE:
+        break;
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+
+        TextOut(hdc, rand() % 600, rand() % 400, str, _tcslen(str));
+
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_CHAR:
+        str[0] = wParam;
+        str[1] = NULL;
+        InvalidateRgn(hWnd, NULL, TRUE);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
