656068c418c4c8f1f1e77b7eee50da8a94528c81
BOOK-36O9DT5K00\wonyong <r00tdr4g0n@gmail.com>
2024-05-20 00:04:03 +0900

    [add] Chapter02 - Exercise06 추가

---
+++ Chapter02/Exercise06/Exercise06/Exercise06.cpp
@@ -0,0 +1,82 @@
+﻿#include "framework.h"
+#include "Exercise06.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+WCHAR szWindowClass[] = L"WindowClass Name";
+WCHAR szTitle[] = L"Chapter02 Exercise06";
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    HWND hWnd;
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    RegisterClassExW(&wcex);
+
+    hWnd = CreateWindow(szWindowClass,
+        szTitle,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static HPEN pen = NULL, oldPen = NULL;
+    static HBRUSH brush = NULL, oldBrush = NULL;
+    HDC hdc = NULL;
+    PAINTSTRUCT ps = { 0, };
+
+    switch (message) {
+    case WM_CREATE:
+        pen = CreatePen(PS_SOLID, 1, RGB(0xff, 0xff, 00));
+        brush = CreateSolidBrush(RGB(0xff, 0x66, 0xff));
+        break;
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+
+        oldPen = (HPEN)SelectObject(hdc, pen);
+        oldBrush = (HBRUSH)SelectObject(hdc, brush);
+
+        Ellipse(hdc, 190, 110, 210, 130);
+
+        SelectObject(hdc, oldPen);
+        SelectObject(hdc, oldBrush);
+
+        EndPaint(hWnd, &ps);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
