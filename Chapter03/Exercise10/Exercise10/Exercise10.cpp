515a345baeda169b1fb342dcb5d14b02e80dbc3c
WYCHOI-DESK\wychoi <r00tdr4g0n@gmail.com>
2024-06-24 02:21:37 +0900

    [mod] Exercise10 수정
    1. 맵 그리기 함수 추가
    2. 먹이 그리기 함수 추가

--- Chapter03/Exercise10/Exercise10/Exercise10.cpp
+++ Chapter03/Exercise10/Exercise10/Exercise10.cpp
@@ -1,10 +1,21 @@
 #include "framework.h"
 #include "Exercise10.h"
+#include <time.h>
+
+#define RADIUS 10
+#define MAP_X_SIZE 30
+#define MAP_Y_SIZE 30
+#define FRUIT_CNT 5
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 WCHAR szWindowClass[] = L"WindowClass Name";
 WCHAR szTitle[] = L"Window Title Name";
+
+typedef struct POS {
+    int x;
+    int y;
+} POS;
 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
@@ -48,21 +59,68 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmd
     return (int)msg.wParam;
 }
 
+void DrawMap(HDC a_hdc)
+{
+    for (int i = 0; i < MAP_X_SIZE; i++) {
+        Rectangle(a_hdc, i * 20, 0, i * 20 + RADIUS * 2, RADIUS * 2);
+    }
+    
+    for (int i = 1; i < MAP_Y_SIZE - 1; i++) {
+        Rectangle(a_hdc, 0, i * (RADIUS * 2), RADIUS * 2, i * (RADIUS * 2) + (RADIUS * 2));
+        Rectangle(a_hdc, (MAP_X_SIZE - 1) * (RADIUS * 2), i * (RADIUS * 2), (MAP_X_SIZE - 1) * (RADIUS * 2) + (RADIUS * 2), i * (RADIUS * 2) + (RADIUS * 2));
+    }
+
+    for (int i = 0; i < MAP_X_SIZE; i++) {
+        Rectangle(a_hdc, i * 20, (MAP_Y_SIZE - 1) * (RADIUS * 2), i * 20 + RADIUS * 2, (MAP_Y_SIZE - 1) * (RADIUS * 2) + RADIUS * 2);
+    }
+}
+
+void DrawFruit(HDC a_hdc, POS* a_fruit, UINT a_size)
+{
+
+    SelectObject(a_hdc, GetStockObject(BLACK_BRUSH));
+
+    for (int i = 0; i < a_size; i++) {
+        Ellipse(a_hdc, 
+            a_fruit[i].x * RADIUS * 2, a_fruit[i].y * RADIUS * 2,
+            a_fruit[i].x * RADIUS * 2 + RADIUS * 2, a_fruit[i].y * RADIUS * 2 + RADIUS * 2
+        );
+    }
+
+    SelectObject(a_hdc, GetStockObject(WHITE_BRUSH));
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     HDC hdc;
     PAINTSTRUCT ps;
     static int x, y;
+    static POS fruit[FRUIT_CNT];
 
     switch (message) {
     case WM_CREATE:
         x = 20;
         y = 20;
+
+        srand((UINT)time(NULL));
+
+        for (int i = 0; i < FRUIT_CNT; i++) {
+            fruit[i].x = (rand() % (MAP_X_SIZE - 1)) + 1;
+            fruit[i].y = (rand() % (MAP_Y_SIZE - 1)) + 1;
+
+            for (int j = 0; j < i; j++) {
+                if (fruit[i].x == fruit[j].x && fruit[i].y == fruit[j].y) {
+                    i--;
+                }
+            }
+        }
+
         break;
     case WM_PAINT:
         hdc = BeginPaint(hWnd, &ps);
 
-        Ellipse(hdc, x - 20, y - 20, x + 20, y + 20);
+        DrawMap(hdc);
+        DrawFruit(hdc, fruit, FRUIT_CNT);
 
         EndPaint(hWnd, &ps);
         break;
