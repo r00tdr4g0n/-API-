7f33a137e4457bc9831923da326cd0a6df7398dc
WYCHOI-DESK\wychoi <r00tdr4g0n@gmail.com>
2024-06-25 02:05:49 +0900

    [mod] Exercise10 수정
    뱀 그리기 추가

--- Chapter03/Exercise10/Exercise10/Exercise10.cpp
+++ Chapter03/Exercise10/Exercise10/Exercise10.cpp
@@ -6,6 +6,7 @@
 #define MAP_X_SIZE 30
 #define MAP_Y_SIZE 30
 #define FRUIT_CNT 5
+#define MAX_SNAKE_LEN 200
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -90,23 +91,63 @@ void DrawFruit(HDC a_hdc, POS* a_fruit, UINT a_size)
     SelectObject(a_hdc, GetStockObject(WHITE_BRUSH));
 }
 
+void DrawSnake(HDC a_hdc, POS* a_snake, UINT a_len)
+{
+    HBRUSH redBr = CreateSolidBrush(RGB(255, 0, 0));
+    HBRUSH blueBr = CreateSolidBrush(RGB(0, 0, 255));
+    HBRUSH oldBr;
+
+    oldBr = (HBRUSH)SelectObject(a_hdc, redBr);
+
+    Ellipse(a_hdc,
+        a_snake[0].x * RADIUS * 2, a_snake[0].y * RADIUS * 2,
+        a_snake[0].x * RADIUS * 2 + RADIUS * 2, a_snake[0].y * RADIUS * 2 + RADIUS * 2
+    );
+
+    oldBr = (HBRUSH)SelectObject(a_hdc, blueBr);
+
+    for (int i = 1; i < a_len; i++) {
+        Ellipse(a_hdc,
+            a_snake[i].x * RADIUS * 2, a_snake[i].y * RADIUS * 2,
+            a_snake[i].x * RADIUS * 2 + RADIUS * 2, a_snake[i].y * RADIUS * 2 + RADIUS * 2
+        );
+    }
+
+    SelectObject(a_hdc, oldBr);
+
+    DeleteObject(redBr);
+    DeleteObject(blueBr);
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     HDC hdc;
     PAINTSTRUCT ps;
     static int x, y;
     static POS fruit[FRUIT_CNT];
+    static POS snake[MAX_SNAKE_LEN];
+    static UINT uiLen;
+    static bool bStart;
 
     switch (message) {
     case WM_CREATE:
+        bStart = false;
+
         x = 20;
         y = 20;
+
+        snake[0] = { MAP_X_SIZE / 2 + 1, MAP_Y_SIZE / 2 };
+        snake[1] = { MAP_X_SIZE / 2, MAP_Y_SIZE / 2 };
+        snake[2] = { MAP_X_SIZE / 2 - 1, MAP_Y_SIZE / 2 };
+        snake[3] = { MAP_X_SIZE / 2 - 2, MAP_Y_SIZE / 2 };
+
+        uiLen = 4;
 
         srand((UINT)time(NULL));
 
         for (int i = 0; i < FRUIT_CNT; i++) {
-            fruit[i].x = (rand() % (MAP_X_SIZE - 1)) + 1;
-            fruit[i].y = (rand() % (MAP_Y_SIZE - 1)) + 1;
+            fruit[i].x = (rand() % (MAP_X_SIZE - 2)) + 1;
+            fruit[i].y = (rand() % (MAP_Y_SIZE - 2)) + 1;
 
             for (int j = 0; j < i; j++) {
                 if (fruit[i].x == fruit[j].x && fruit[i].y == fruit[j].y) {
@@ -121,32 +162,41 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
         DrawMap(hdc);
         DrawFruit(hdc, fruit, FRUIT_CNT);
+        DrawSnake(hdc, snake, uiLen);
 
         EndPaint(hWnd, &ps);
         break;
     case WM_KEYDOWN:
         switch (wParam)
         {
-        case VK_LEFT:
-            x -= 40;
-            break;
-        case VK_UP:
-            y -= 40;
-            break;
-        case VK_RIGHT:
-            x += 40;
-            break;
-        case VK_DOWN:
-            y += 40;
-            break;
-        case VK_HOME:
-            x = 20;
-            y = 20;
+        case VK_RETURN:
+            if (!bStart) {
+                bStart = true;
+                SetTimer(hWnd, 1, 500, NULL);
+            }
+            else {
+                bStart = false;
+                KillTimer(hWnd, 1);
+            }
+
             break;
         default:
             break;
         }
         InvalidateRgn(hWnd, NULL, TRUE);
+        break;
+    case WM_TIMER:
+        if (bStart) {
+            for (int i = 0; i < uiLen - 1; i++) {
+                snake[uiLen - 1 - i].x = snake[uiLen - 2 - i].x;
+                snake[uiLen - 1 - i].y = snake[uiLen - 2 - i].y;
+            }
+
+            snake[0].x++;
+
+            InvalidateRgn(hWnd, NULL, TRUE);
+        }
+
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
